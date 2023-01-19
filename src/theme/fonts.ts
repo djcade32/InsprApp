fbd2b200530e5182b106ef152d242e49adb420c9
Norman Cade <norman_cade32@yahoo.com>
2023-01-18 19:47:46 -0500

    configure project to use custom fonts

---
+++ src/theme/fonts.ts
@@ -0,0 +1,16 @@
+const primaryFontFamily = 'Lato';
+
+const size = {
+  sm: 18,
+  md: 20,
+  lg: 24,
+  xlg: 32,
+};
+
+const weight = {
+  light: '300',
+  bold: 'bold',
+  medium: '500',
+};
+
+export default {size, weight, primaryFontFamily};
