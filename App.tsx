10b941186ddf12389109bad7a1ee67174bdffbed
Norman Cade <norman_cade32@yahoo.com>
2023-02-01 17:11:14 -0500

    Connect aws backend

--- App.tsx
+++ App.tsx
@@ -2,6 +2,10 @@ import React from 'react';
 import {SafeAreaView} from 'react-native';
 import {MenuProvider} from 'react-native-popup-menu';
 import Navigation from './src/navigation';
+import {Amplify} from 'aws-amplify';
+import awsconfig from './src/aws-exports';
+
+Amplify.configure(awsconfig);
 
 const App = () => {
   return (
